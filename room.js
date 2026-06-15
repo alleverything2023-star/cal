86c03cce5c2d98b512e1b4a475fe773185ec7371
alleverything2023-star <all.everything2023@gmail.com>
2026-06-15 10:53:02 +0900

    Update room.js

--- room.js
+++ room.js
@@ -1,45 +1,23 @@
-import {
-ref,
-set,
-onValue,
-onDisconnect,
-serverTimestamp
-}
-from "https://www.gstatic.com/firebasejs/12.14.0/firebase-database.js";
-
+import { ref, set, onValue, onDisconnect } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-database.js";
 import { db } from "./firebase.js";
 
 export let myId = null;
 
-export async function joinRoom(name){
+export async function joinRoom(name) {
+  myId = crypto.randomUUID();
 
-myId = crypto.randomUUID();
+  await set(ref(db, "participants/" + myId), {
+    name,
+    joinedAt: Date.now()
+  });
 
-await set(
-ref(db,"participants/" + myId),
-{
-name,
-joinedAt:Date.now()
-}
-);
-
-onDisconnect(
-ref(db,"participants/" + myId)
-).remove();
-
+  // タブを閉じたり回線が切れたら自動でFirebaseから削除
+  onDisconnect(ref(db, "participants/" + myId)).remove();
 }
 
-export function listenParticipants(callback){
-
-onValue(
-ref(db,"participants"),
-snapshot=>{
-
-const data =
-snapshot.val() || {};
-
-callback(data);
-
-});
-
+export function listenParticipants(callback) {
+  onValue(ref(db, "participants"), snapshot => {
+    const data = snapshot.val() || {};
+    callback(data);
+  });
 }
